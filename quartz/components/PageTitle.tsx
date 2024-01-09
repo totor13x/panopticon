4f5af854b95b9867f8c7a08e1c1970f5ae4ec718
totor13x <totor_@outlook.com>
2024-01-09 13:24:04 +0400

    Лишний H1

--- quartz/components/PageTitle.tsx
+++ quartz/components/PageTitle.tsx
@@ -11,9 +11,9 @@ function PageTitle({ fileData, cfg, displayClass }: QuartzComponentProps) {
           <path d="M 5 8 A 2.0002 2.0002 0 1 0 5 12 L 45 12 A 2.0002 2.0002 0 1 0 45 8 L 5 8 z M 5 23 A 2.0002 2.0002 0 1 0 5 27 L 45 27 A 2.0002 2.0002 0 1 0 45 23 L 5 23 z M 5 38 A 2.0002 2.0002 0 1 0 5 42 L 45 42 A 2.0002 2.0002 0 1 0 45 38 L 5 38 z"></path>
         </svg>
       </div>
-      <h1 class={`page-title ${displayClass ?? ""}`}>
+      <div class={`page-title ${displayClass ?? ""}`}>
         <a href={baseDir}>{title}</a>
-      </h1>
+      </div>
     </div>
   )
 }
@@ -21,6 +21,7 @@ function PageTitle({ fileData, cfg, displayClass }: QuartzComponentProps) {
 PageTitle.css = `
 .page-title {
   margin: 0;
+  font-size: 1.75rem;
 }
 `
 
